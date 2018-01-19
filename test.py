3eaffaac87c6bb941c05512528dc34c8e0de95da
shinochin <kazutoshi.shinoda0516@gmail.com>
2018-01-19 15:15:23 +0900

    Update test.py

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ from utils import padding
 EMB_DIM = 32 # embedding dimension
 HIDDEN_DIM = 32 # hidden state dimension of lstm cell
 SEQ_LENGTH = 17 # sequence length
-COND_LENGTH = 7 # condition length
+COND_LENGTH = 1 # condition length
 START_TOKEN = 0
 SEED = 88
 BATCH_SIZE = 16
@@ -29,30 +29,36 @@ dis_dropout_keep_prob = 0.75
 dis_l2_reg_lambda = 0.2
 dis_batch_size = 16
 
+#########################################################################################
+#  Other Hyper-parameters
+#########################################################################################
+path_to_token2id = 'save/conddis_token2id.pickle'
+path_to_generator = 'save/conddis_haiku_generator'
 
-def main():
-    with open('save/token2id.pickle') as f:
+
+def get_haiku_conddis(kigo):
+    with open(path_to_token2id) as f:
         token2id = pickle.load(f)
     vocab_size=len(token2id)
     UNK = token2id.get('<UNK>', 0)
     
     generator = Generator(vocab_size, BATCH_SIZE, EMB_DIM, HIDDEN_DIM, SEQ_LENGTH, COND_LENGTH, START_TOKEN, is_cond=1)
-    discriminator = Discriminator(sequence_length=SEQ_LENGTH, num_classes=2, vocab_size=vocab_size, embedding_size=dis_embedding_dim, 
-                                filter_sizes=dis_filter_sizes, num_filters=dis_num_filters, l2_reg_lambda=dis_l2_reg_lambda)
+    discriminator = Discriminator(sequence_length=SEQ_LENGTH, cond_length=COND_LENGTH, num_classes=2, vocab_size=vocab_size, embedding_size=dis_embedding_dim, 
+                                filter_sizes=dis_filter_sizes, num_filters=dis_num_filters, l2_reg_lambda=dis_l2_reg_lambda, is_cond=1)
     
     sess = tf.Session()
     saver = tf.train.Saver()
-    saver.restore(sess, 'save/haiku_generator')
-    kigo = [u"霧"]
-    cond = map(lambda x: token2id.get(x, 0), [u"霧"])
+    saver.restore(sess, path_to_generator)
+    
+    if isinstance(kigo, str):
+        kigo = kigo.decode('utf-8')
+    cond = map(lambda x: token2id.get(x, 0), [kigo])
     cond = np.array(padding(cond, COND_LENGTH, UNK)*BATCH_SIZE).reshape(BATCH_SIZE, COND_LENGTH)
     generated_sequences = generator.generate(sess, cond=cond)
     
     id2token = {k:v for v,k in token2id.items()}
     generated_haikus = map(lambda y: map(lambda x: id2token.get(x, '<UNK>'), y), generated_sequences)
     
-    for haiku in generated_haikus:
-        print(re.sub(r' <UNK>', '',' '.join(haiku)))
+    generated_haikus = map(lambda x: re.sub(r' <UNK>', '',' '.join(x)), generated_haikus)
     
-if __name__ == '__main__':
-    main()
+    return generated_haikus
